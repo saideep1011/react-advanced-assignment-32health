453bbece4a2c6474cde7650960d3304cde5607d6
saideep1011 <saideep2311@gmail.com>
2025-02-22 23:00:27 +0530

    UI changes and avatars added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { MailOutlined, PhoneOutlined, GlobalOutlined } from "@ant-design/icons";
+import {
+  MailOutlined,
+  PhoneOutlined,
+  GlobalOutlined,
+  HeartOutlined,
+  EditOutlined,
+  DeleteOutlined,
+} from "@ant-design/icons";
 import "./App.css";
 
 function App() {
@@ -30,22 +37,19 @@ function App() {
     return <p className="text-center mt-8 text-red-500">Error: {error}</p>;
 
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+    <div className="min-h-screen  bg-white p-3 md:p-4">
+      <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-4 gap-6 ">
         {users.map((user) => (
-          <div
-            key={user.id}
-            className="bg-blue-500 rounded-lg  shadow-sm hover:shadow-md transition-shadow"
-          >
-            <div className="flex justify-center items-center h-40 bg-red-500">
+          <div key={user.id} className=" bg-gray-100 border">
+            <div className="flex justify-center items-center h-48">
               <img
-                src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
+                src={`https://api.dicebear.com/7.x/avataaars/svg?seed=${user.username}&mood[]=happy`}
                 alt={user.name}
-                className="h-full w-auto  shadow-lg"
+                className="h-full bg-gray-200 w-auto "
               />
             </div>
-
-            <div className="space-y-3 bg-green-500 p-2">
+            {/* User Data Section */}
+            <div className="space-y-3 bg-white p-2">
               <h2 className="text-xl font-semibold mb-4 text-gray-800 ">
                 {user.name}
               </h2>
@@ -56,9 +60,7 @@ function App() {
 
               <div className="flex items-center gap-2 text-gray-600">
                 <PhoneOutlined className="text-lg" />
-                <span className="text-sm">
-                  {user.phone} • s{user.id}
-                </span>
+                <span className="text-sm">{user.phone}</span>
               </div>
 
               <div className="flex items-center gap-2 text-gray-600">
@@ -71,6 +73,17 @@ function App() {
                 >
                   http://{user.website}
                 </a>
+              </div>
+            </div>
+            <div className="grid grid-cols-3 items-center p-4 bg-gray-50">
+              <div className="flex justify-center items-center">
+                <HeartOutlined className="text-red-500 text-xl cursor-pointer" />
+              </div>
+              <div className="flex justify-center items-center border-l border-r border-gray-300">
+                <EditOutlined className="text-gray-500 text-xl cursor-pointer" />
+              </div>
+              <div className="flex justify-center items-center">
+                <DeleteOutlined className="text-gray-500 text-xl cursor-pointer" />
               </div>
             </div>
           </div>
